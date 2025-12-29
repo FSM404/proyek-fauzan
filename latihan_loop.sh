4751d6f974b35f97a78d0fcd505376e7b3ae732a
Fauzan <fauzan.sammy5@smk.belajar.id>
2025-12-29 10:37:07 +0700

    Menambahkan latihan loop

---
+++ latihan_loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo  "Hello, World!"
+
+
+echo  "---Mulai For Loop---"
+for i in {1..5}
+do
+  echo "Angka ke-$i"
+done
+echo "---Mulai Infinite Loop (Tekan Ctrl+C untuk stop)---"
+while true
+do
+echo "Running..."
+sleep 1
+done
+
