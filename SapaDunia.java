70ccf38109c9235e5654604e83202b904498d97b
Fauzan <fauzan.sammy5@smk.belajar.id>
2025-12-29 17:15:00 +0700

    Update SapaDunia jadi interaktif

--- SapaDunia.java
+++ SapaDunia.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class SapaDunia {
-public static void main (String[] args){
-System.out.println("Halo, Fauzan! Selamat datang di Java.");
+
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+
+System.out.print("Siapa nama kamu? ");
+String nama = input.nextLine();
+System.out.println("Halo,  " + nama + "! Ternyata Java itu asik ya.");
+
 }
 
 
